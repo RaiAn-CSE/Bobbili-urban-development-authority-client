b16c6fdb07025c76a4d51844498d08b4128cf8ed
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-09-05 19:42:38 +0600

    working on checklist- question logic about to done 95%

--- src/Pages/LPT/Darft-Application/Application-Checklist/ApplicationChecklist.jsx
+++ src/Pages/LPT/Darft-Application/Application-Checklist/ApplicationChecklist.jsx
@@ -1,42 +1,50 @@
-import { useState } from "react";
-import ChecklistQuestions from "./Questions.json"
+import React, { useState } from "react";
+import ChecklistQuestions from "./Questions.json";
 
 function ApplicationChecklist() {
-    const [Questions, setQuestions] = useState(ChecklistQuestions.Questions);
-    const handleAnswer = (event) => {
-        const { name, value } = event.target;
+    const [questions, setQuestions] = useState(ChecklistQuestions.Questions);
 
-        console.log(name, value)
-        const updated = Questions.map(data => {
-            console.log( typeof(name))
-         return   { ...data, answer: (data.no == name) ? value === "yes" ? true : false : "" }});
-            console.log(updated)
-        return setQuestions(updated)
-    }
+    const handleAnswer = (event, questionNo) => {
+        const updatedQuestions = questions.map((question) => ({
+            ...question,
+            answer: question.no === questionNo ? event.target.value : question.answer,
+        }));
+        setQuestions(updatedQuestions);
+    };
+
     return (
         <div>
-            {
-                Questions.map(data => {
-                    const { no, question, answer } = data;
-                    return (
-                        <div key={no}>
-                            <p>{no}. {question}</p>
-                            <div>
-                                <label htmlFor="">
-                                    <input onChange={(e) => handleAnswer(e)} value="yes" type="radio" name={no} className="radio radio-success" />
-                                    Yes
-                                </label>
-                                <label htmlFor="">
-                                    <input onChange={(e) => handleAnswer(e)} value="no" type="radio" name={no} className="radio radio-success" />
-                                    No
-                                </label>
-                            </div>
-                        </div>
-                    )
-                })
-            }
+            {questions.map(({ no, question, answer }) => (
+                <div key={no} className="">
+                    <p>{no}. {question}</p>
+                    <div>
+                        <label>
+                            <input
+                                type="radio"
+                                name={no}
+                                value="yes"
+                                className="radio radio-success"
+                                checked={answer === "yes"}
+                                onChange={(event) => handleAnswer(event, no)}
+                            />
+                            Yes
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name={no}
+                                value="no"
+                                className="radio radio-success"
+                                checked={answer === "no"}
+                                onChange={(event) => handleAnswer(event, no)}
+                            />
+                            No
+                        </label>
+                    </div>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default ApplicationChecklist;
