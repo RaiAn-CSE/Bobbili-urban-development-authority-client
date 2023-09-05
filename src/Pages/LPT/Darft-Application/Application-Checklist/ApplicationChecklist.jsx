4014ba54a6c8f895ebc538a4a89b97689f6b67d4
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-09-05 19:12:17 +0600

    working on checklist- question logic about to done

--- src/Pages/LPT/Darft-Application/Application-Checklist/ApplicationChecklist.jsx
+++ src/Pages/LPT/Darft-Application/Application-Checklist/ApplicationChecklist.jsx
@@ -1,35 +1,51 @@
-import { useState } from "react";
-import ChecklistQuestions from "./Questions.json"
+import React, { useState } from "react";
+import ChecklistQuestions from "./Questions.json";
 
 function ApplicationChecklist() {
-    const [Questions, setQuestions] = useState(ChecklistQuestions.Questions);
-    const handleAnswer = (event) => {
-        console.log(event.target)
-    }
-    return (
-        <div>
-            {
-                Questions.map(data => {
-                    const { no, question, answer } = data;
-                    return (
-                        <div key={no}>
-                            <p>{question}</p>
-                            <div>
-                                <label htmlFor="">
-                                    <input onChange={(event) => handleAnswer(event)} type="radio" name={no} className="radio radio-success" />
-                                    Yes
-                                </label>
-                                <label htmlFor="">
-                                    <input onChange={(event) => handleAnswer(event)} type="radio" name={no} className="radio radio-success" />
-                                    No
-                                </label>
-                            </div>
-                        </div>
-                    )
-                })
-            }
+  const [questions, setQuestions] = useState(ChecklistQuestions.Questions);
+
+  const handleAnswer = (event, questionNo) => {
+    const updatedQuestions = questions.map((question) => ({
+      ...question,
+      answer: question.no === questionNo ? event.target.value : question.answer,
+    }));
+    console.log(updatedQuestions,"UpdatedQuestions")
+    setQuestions(updatedQuestions);
+  };
+
+  return (
+    <div>
+      {questions.map(({ no, question, answer }) => (
+        <div key={no}>
+          <p>{question}</p>
+          <div>
+            <label>
+              <input
+                type="radio"
+                name={no}
+                value="yes"
+                className="radio radio-success"
+                checked={answer === "yes"}
+                onChange={(event) => handleAnswer(event, no)}
+              />
+              Yes
+            </label>
+            <label>
+              <input
+                type="radio"
+                name={no}
+                value="no"
+                className="radio radio-success"
+                checked={answer === "no"}
+                onChange={(event) => handleAnswer(event, no)}
+              />
+              No
+            </label>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default ApplicationChecklist;
