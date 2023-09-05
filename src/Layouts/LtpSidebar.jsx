9dc4e975bad3def6484f726663072c8cbb293595
Ami.Raian <ami.robirai@gmail.com>
2023-09-05 19:55:51 +0600

    Ltp dashboard

---
+++ src/Layouts/LtpSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LtpSidebar = () => {
+    return (
+        <div>
+            <h2>THis is sidebar LTP for </h2>
+        </div>
+    );
+};
+
+export default LtpSidebar;
