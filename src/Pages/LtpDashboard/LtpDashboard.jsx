b512e7cf3d14d02e7ab28147949b1c061c7ce943
Ami.Raian <ami.robirai@gmail.com>
2023-09-05 20:21:53 +0600

    tailwind setup

--- src/Pages/LtpDashboard/LtpDashboard.jsx
+++ src/Pages/LtpDashboard/LtpDashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LtpDashboard = () => {
     return (
         <div>
-            <h1>this is Ltp Main Dashboard Created</h1>
+            <h1 className='text-xl'>this is Ltp Main Dashboard Created</h1>
         </div>
     );
 };
