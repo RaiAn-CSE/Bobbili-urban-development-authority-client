cee426e341f76577b83b46a59f1a3c39b81ac327
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-09-05 18:55:16 +0600

    working on checklist radio button logic

--- src/Pages/LPT/Darft-Application/Application-Checklist/ApplicationChecklist.jsx
+++ src/Pages/LPT/Darft-Application/Application-Checklist/ApplicationChecklist.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import ChecklistQuestions from "./Questions.json"
 
-function ApplicatinChecklist() {
+function ApplicationChecklist() {
+    const [Questions, setQuestions] = useState(ChecklistQuestions.Questions);
+    const handleAnswer = (event) => {
+        console.log(event.target)
+    }
     return (
         <div>
-
+            {
+                Questions.map(data => {
+                    const { no, question, answer } = data;
+                    return (
+                        <div key={no}>
+                            <p>{question}</p>
+                            <div>
+                                <label htmlFor="">
+                                    <input onChange={(event) => handleAnswer(event)} type="radio" name={no} className="radio radio-success" />
+                                    Yes
+                                </label>
+                                <label htmlFor="">
+                                    <input onChange={(event) => handleAnswer(event)} type="radio" name={no} className="radio radio-success" />
+                                    No
+                                </label>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
 
-export default ApplicatinChecklist;
+export default ApplicationChecklist;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../Pages/Login/Login";
+import ApplicationChecklist from "../Pages/LPT/Darft-Application/Application-Checklist/ApplicationChecklist";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: <ApplicationChecklist />,
   },
 ]);
 
