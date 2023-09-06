993f24c427c90182c144b27e8b25da6844cd79c8
Ami.Raian <ami.robirai@gmail.com>
2023-09-06 12:17:49 +0600

    staper interface ready

--- src/Pages/LtpDashboard/DraftApplication.jsx
+++ src/Pages/LtpDashboard/DraftApplication.jsx
@@ -1,14 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Table from '../Components/Table';
 
 const DraftApplication = () => {
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const steps = [
+        "Register",
+        "Choose plan",
+        "Purchase",
+        "Receive Product"
+    ];
+
+    const handleStepClick = (index) => {
+        setCurrentStep(index);
+    };
+
+    const stepClasses = (index) => {
+        if (index === currentStep) {
+            return 'step step-primary';
+        } else if (index < currentStep) {
+            return 'step step-primary';
+        } else {
+            return 'step';
+        }
+    };
+
+    const btnClass = 'bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow';
+
+    // Render the form based on the current step
+    const renderStepContent = () => {
+        switch (currentStep) {
+            case 0:
+                return (
+                    <div>
+                        <h2>Step 1: Register</h2>
+                        <>
+                            <Table />
+                        </>
+                    </div>
+                );
+            case 1:
+                return (
+                    <div>
+                        <h2>Step 2: Choose plan</h2>
+                        <>
+                            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat odio sed molestiae consequuntur odit ex reiciendis, voluptate mollitia voluptas rem.</h1>
+                        </>
+                    </div>
+                );
+            case 2:
+                return (
+                    <div>
+                        <h2>Step 3: Purchase</h2>
+                        <>
+                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, quam doloribus quis natus nesciunt tempora eligendi. Cumque, voluptatibus. Illo officiis mollitia minus optio porro illum atque neque ipsum corporis itaque?</h1>
+                        </>
+                    </div>
+                );
+            case 3:
+                return (
+                    <div>
+                        <h2>Step 4: Receive Product</h2>
+                        <>
+                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nisi corporis repellat ratione maiores? Repellat harum nemo neque nesciunt? Quibusdam quas molestias suscipit numquam sapiente?</h1>
+                        </>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
+
     return (
-        <ul className="steps">
-            <li className="step step-primary">Register</li>
-            <li className="step step-primary">Choose plan</li>
-            <li className="step step-primary">Purchase</li>
-            <li className="step">Receive Product</li>
-        </ul>
+        <>
+            <ul className="steps steps-vertical lg:steps-horizontal">
+                {steps.map((step, index) => (
+                    <li key={index} className={stepClasses(index)} onClick={() => handleStepClick(index)}>
+                        {step}
+                    </li>
+                ))}
+            </ul>
+
+            {renderStepContent()}
+
+            <div className="buttons">
+                <button className={btnClass} onClick={() => setCurrentStep(currentStep - 1)} disabled={currentStep === 0}>
+                    Previous
+                </button>
+                <button className={btnClass} onClick={() => setCurrentStep(currentStep + 1)} disabled={currentStep === steps.length - 1}>
+                    Next
+                </button>
+            </div>
+        </>
     );
 };
 
