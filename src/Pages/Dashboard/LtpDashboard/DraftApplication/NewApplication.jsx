a5a2dfbe976b737b5ec57c292dfb08d3e4e06cf7
Ami.Raian <ami.robirai@gmail.com>
2023-09-08 01:33:39 +0600

    LTP Dashboard Home static style

--- src/Pages/Dashboard/LtpDashboard/DraftApplication/NewApplication.jsx
+++ src/Pages/Dashboard/LtpDashboard/DraftApplication/NewApplication.jsx
@@ -1,11 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { VscGitPullRequestCreate } from "react-icons/vsc";
 
 const NewApplication = () => {
   return (
-    <div>
-      <p>this is new application</p>
-      <Link to="/dashboard/draftApplication/buildingInfo">Next</Link>
+    <div className="relative my-3">
+      <Link className="absolute right-3 border-2 rounded-lg p-1 bg-gray-300" to="/dashboard/draftApplication/buildingInfo">
+        <span className="flex items-center">
+          <span className="mr-2">Create new</span>
+          <VscGitPullRequestCreate color='#2c8877' />
+        </span>
+      </Link>
+
+      <div className="overflow-x-auto w-full absolute top-9">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Sl.no.</th>
+              <th>Application no.</th>
+              <th>Owner name</th>
+              <th>Phone no.</th>
+              <th>Case type</th>
+              <th>Village</th>
+              <th>Mandal</th>
+              <th>Created date</th>
+              <th>Button</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1.</th>
+              <td>1177/XX/001/BUDA/2023</td>
+              <td>XXXX XXX</td>
+              <td>99xxxxxxx99</td>
+              <td>New</td>
+              <td>Piridi</td>
+              <td>Bobbili</td>
+              <td>07-04-2023</td>
+              <td><button className="btn btn-error">Delete</button></td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>2.</th>
+              <td>1177/XX/001/BUDA/2023</td>
+              <td>XXXX XXX</td>
+              <td>99xxxxxxx99</td>
+              <td>New</td>
+              <td>Piridi</td>
+              <td>Bobbili</td>
+              <td>07-04-2023</td>
+              <td><button className="btn btn-error">Delete</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
