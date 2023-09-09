5efc52626a0fae998077524f843d7ee855985130
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-09-09 08:18:50 +0600

    Dynamic search method applied

--- src/Pages/Dashboard/Admin/Admin2/AllUsers.jsx
+++ src/Pages/Dashboard/Admin/Admin2/AllUsers.jsx
@@ -1,39 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IndividualUser from "./IndividualUser";
 import tableStyle from "../../../../Style/tableStyle.module.css";
 import { useQuery } from "react-query";
 
 const AllUsers = () => {
-  const { data, refetch, isLoading } = useQuery({
+  const [records, setRecords] = useState([]);
+
+  const { data, refetch, isLoading, isSuccess } = useQuery({
     queryKey: ["allUser"],
     queryFn: async () => {
       const response = await fetch("http://localhost:5000/allUser");
-      const data = response.json();
+      const data = await response.json();
       return data;
     },
   });
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log("success");
+      console.log(data);
+
+      setRecords([...data]);
+    }
+  }, [isSuccess, data]);
+
+  // console.log(records);
+
+  const filter = (event) => {
+    console.log(data);
+    setRecords(data.filter((f) => f?.name?.includes(event.target.value)));
+    console.log(records);
+  };
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
   return (
-    <div className="overflow-x-auto">
-      <table className="table w-[60%] mx-auto mt-16">
-        {/* head */}
-        <thead className="bg-[#b8e994] Roboto font-bold text-black text-center">
-          <tr>
-            <th>Name</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody className="text-center">
-          {data?.map((user) => (
-            <IndividualUser user={user} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      {/* <h1 className="mt-10 text-center text-3xl font-bold Roboto">All Users</h1> */}
+
+      {/* input box for search users  */}
+      <div className="form-control mt-5 w-1/2 mx-auto  max-w-xs">
+        <div>
+          <label
+            for="search"
+            class="block mb-2 text-sm font-bold text-gray-900 dark:text-white"
+          >
+            Search User
+          </label>
+          <input
+            type="text"
+            id="search"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="John"
+            onChange={filter}
+            required
+          />
+        </div>
+      </div>
+
+      {/* display users  */}
+      <div className="overflow-x-auto">
+        <table className="table w-[70%] mx-auto mt-10 border">
+          {/* head */}
+          <thead className="bg-[#C0E9E4] Roboto font-bold text-black text-center">
+            <tr>
+              <th>Name</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {records?.map((user) => (
+              <IndividualUser user={user} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
