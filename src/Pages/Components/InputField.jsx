74cf2772b468aa673524df7d234887a4eb705846
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-09-10 22:36:31 +0600

    Merge all code

--- src/Pages/Components/InputField.jsx
+++ src/Pages/Components/InputField.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const InputField = ({ id, name, placeholder, type, label }) => {
-    // Define default values for type and placeholder if not provided
-    const inputType = type || 'text';
+  // Define default values for type and placeholder if not provided
+  const inputType = type || "text";
 
-    return (
-        <div className="my-2 mx-3">
-            <label htmlFor={id} className="block text-gray-600 mb-1 font-semibold">
-                {label}
-            </label>
-            <input
-                type={inputType} // Use the inputType variable as the type attribute
-                id={id}
-                name={name}
-                placeholder={placeholder} // Use the labelPlaceholder variable as the placeholder attribute
-                className="w-full px-3 py-2 border rounded-lg max-w-xs"
-            />
-        </div>
-    );
+  return (
+    <div className="my-2 mx-3">
+      <label htmlFor={id} className="block text-gray-600 mb-1 font-semibold">
+        {label}
+      </label>
+      <input
+        type={inputType} // Use the inputType variable as the type attribute
+        id={id}
+        name={name}
+        placeholder={placeholder} // Use the labelPlaceholder variable as the placeholder attribute
+        className="w-full px-3 py-2 border rounded-lg max-w-xs"
+      />
+    </div>
+  );
 };
 
 export default InputField;
