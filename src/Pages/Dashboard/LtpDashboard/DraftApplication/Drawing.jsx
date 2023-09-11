c8961f3fe13cb25b868e5f5138c07d03d0ff717c
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-09-11 06:26:35 +0600

    Drawing page responsive completed

--- src/Pages/Dashboard/LtpDashboard/DraftApplication/Drawing.jsx
+++ src/Pages/Dashboard/LtpDashboard/DraftApplication/Drawing.jsx
@@ -4,7 +4,8 @@ import getPostData from "../../../Shared/getPostData";
 
 const Drawing = () => {
   const [selectedFiles, setSelectedFiles] = useState({});
-  const btn = "btn px-6 bg-Primary transition duration-700 hover:bg-btnHover";
+  const btn =
+    "btn btn-md text-xs px-4 md:text-sm md:px-6 bg-Primary transition duration-700 hover:bg-btnHover";
 
   const handleFileChange = (event, eventId) => {
     const file = event?.target?.files[0];
@@ -18,8 +19,10 @@ const Drawing = () => {
   return (
     <div className="text-black h-screen p-5 mt-[5%]">
       {/* AutoCAD Drawing */}
-      <div className="flex items-center px-2 space-y-2 mb-8">
-        <p className="flex-1 pr-3">1. AutoCAD Drawing</p>
+      <div className="flex items-center text-base px-2 space-y-2 mb-8">
+        <p className="flex-1 pr-3">
+          <span className="font-bold">1.</span> AutoCAD Drawing
+        </p>
         <div className=" flex items-center space-x-1 text-sm">
           <label className="cursor-pointer font-bold bg-gray-300 py-2 px-4 rounded-full">
             Upload{" "}
@@ -37,8 +40,10 @@ const Drawing = () => {
       </div>
 
       {/* Drawing PDF */}
-      <div className="flex items-center px-2 space-y-2">
-        <p className="flex-1 pr-3">2. Drawing PDF</p>
+      <div className="flex items-center text-base px-2 space-y-2">
+        <p className="flex-1 pr-3">
+          <span className="font-bold">2.</span> Drawing PDF
+        </p>
         <div className="flex items-center space-x-1 text-sm">
           <label className="cursor-pointer font-bold bg-gray-300 py-2 px-4 rounded-full">
             Upload{" "}
