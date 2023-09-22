fbd15f026de6e32481a88b0a6d31ca4979c3d6e5
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-09-23 03:20:00 +0600

    working on application:Part03 function about to done

--- src/Pages/Dashboard/LtpDashboard/DraftApplication/Application.jsx
+++ src/Pages/Dashboard/LtpDashboard/DraftApplication/Application.jsx
@@ -3,18 +3,28 @@ import ApplicationHeader from '../../../Shared/ApplicationHeader';
 
 function Application({ setOpenApplication }) {
 	const Part01 = {
-		column: { "col2": ["NAME", "DOOR No. / FLAT No.", "ROAD/STREET", "VILLAGE|MANDAL", "CITY/TOWN", "DISTRICT", "E-MAIL", ["MOBILE", "ALTERNATE"]] }
+		column: { "col2": ["NAME", "DOOR No. / FLAT No.", "ROAD/STREET", ["VILLAGE", "MANDAL"], "CITY/TOWN", "DISTRICT", "E-MAIL", ["MOBILE", "ALTERNATE"]] }
 	}
 	const Part02 = ["PLOT NOs", "SANCTIONED LAYOUT NO. / LRS NO", ["SURVEY NO.", "VILLAGE"], "PREMISES / DOOR No.", "ROAD/ STREET", ["WARD NO.", "BLOCK No"], "LOCALITY", ["CIRCLE/DIVISION", "DIVISION"], ["CITY/TOWN", "DISTRICT"]];
 	const Part03 = ["SITE AREA (IN SQ.M)", "NO. OF FLOORS", "FLOOR AREA (IN SQ.M)", "PARKING FLOOR AREA (IN SQ.M)", "USE OF THE BUILDING"];
+	// index0 for part03
+	const row1 = [{ "(a) AS PER DOCUMENTS": "" },{ "(b) AS PER SUBMITTED PLAN": "" },{ "(c) ROAD WIDENING AREA": "" },{ "(d)   NET AREA": "" }];
+	
+	const row2 = [{ "CELLAR": "" },{ "STILT": "" },{ "GROUND": "" },{ "UPPER": "" },{ "FLOOR": "" },{ "TOTAL": "" }];
+	
+	// index4
+	const row5 = [{"INDIVIDUAL RESIDENTIAL/GROUP HOUSING/ COMMERCIAL/INSTITUTIONAL/ROW HOUSING/OTHERS (SPECIFY)":"Hello, This is Tanjimul Islam Sabbir"}]
+
+
 	const Part04 = ["BUILDER / DEVELOPER/ CONSTRUCTION FIRM", "ARCHITECT", "ENGINEER", "STRUCTURAL ENGINEER", "SUPERVISOR/SURVEYOR", "TOWN PLANNER"]
 	// Part01
 	const PhoneTD = ["d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d"];
 	const CityTown = ["PIN", "", "", "", "", "", ""];
 	const Village = ["", ""];
 	// Part02
-	const part01SubArray = [3, 4, 7]
+	const part01SubArray = [3, 4, 7];
 	const part02SubArray = [2, 5, 7, 8];
+	const part03SubArray = [0, 1, 4];
 
 	useEffect(() => {
 		// Opening the modal when the component mounts
@@ -67,6 +77,35 @@ function Application({ setOpenApplication }) {
 			</td>
 		)
 	}
+	// Part03
+	const ColDataShow03 = (data, index) => {
+		let colData;
+		if (index === 0) {
+			colData = row1;
+		} else if (index === 1) {
+			colData = row2;
+		} else if (index == 4) {
+			colData = row5;
+		} else {
+			colData = []
+		}
+
+		return (
+			<td className='w-full bg-white border border-black p-0'>{
+				part03SubArray.includes(index) ?
+					<p className='flex '>
+						{colData.map((d, i) =>
+							<p className={`w-full border-black pt-4 border-l ${i == 0 && "border-l-0"}`}>
+								<p className='px-2'>{Object.keys(colData[i])[0]}</p>
+								{/* {Object.values(colData[i])[0]} */}
+								<p className={`${index!==4?"border-t ":"font-bold inline-block border-b-2 border-dotted border-black underline-offset-4 mt-2 ml-2"} border-black px-2 pt-2`}>{i+1}</p>
+							</p>)}
+					</p>
+					:
+					<p className='py-4 px-2'></p>}
+			</td>
+		)
+	}
 	return (
 		<div className='w-full h-full text-black'>
 			<dialog id="my_modal_5" className="modal">
@@ -96,13 +135,16 @@ function Application({ setOpenApplication }) {
 												<td className='bg-white border border-black w-36 p-0'>
 													{index == 7 ?
 														<p className='flex items-center justify-between py-0 pl-2'>
-															<p className='w-1/2'>PHONE</p>
+															<p className={`w-1/2`}>PHONE</p>
 
 															<p className='flex flex-col border-black p-0'>
-																{data.map((d, i) => <p className={`border-l py-[5px] border-black px-2 ${i == 0 && "border-b"}`}>{d}</p>)}
+																{data?.map((d, i) => <p className={`border-l py-[5px] border-black px-2 ${i == 0 && "border-b"}`}>{d}</p>)}
 															</p>
+														</p> : <> {index == 3 ? <p className='flex '>
+															{data.map((d, i) =>
+																<p className={`border-black py-3 px-2 ${i == 0 && "w-1/2 border-r"}`}>{d}</p>)}
 														</p> :
-														<p className='py-4 px-2'>{data}</p>}
+															<p className='py-4 px-2'>{data}</p>}</>}
 												</td>
 												{/* col-03+Row-08  */}
 												{ColDataShow01(data, index)}
@@ -166,8 +208,9 @@ function Application({ setOpenApplication }) {
 									<>
 										<tr key={index} className='bg-white'>
 											<th className='bg-white border border-black w-14'>{index + 1}</th>
-											<td className='bg-white border border-black w-64 p-4'>{data}</td>
-											<td className='bg-white border border-black'></td>
+											<td className='bg-white border border-black w-32 p-4'>{data}</td>
+											{/* <td className='bg-white border border-black'></td> */}
+											{ColDataShow03(data, index)}
 										</tr>
 									</>
 								))}
