5a4f7449e52adf101dc2f46b18e5f6b67ff8d50f
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-09-25 15:43:52 +0600

    Document page upload button design and input validation completed

--- src/Pages/Dashboard/LtpDashboard/DraftApplication/Documents.jsx
+++ src/Pages/Dashboard/LtpDashboard/DraftApplication/Documents.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import Documents from "../../../../assets/Documents.json";
-import { useOutletContext } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 import toast from "react-hot-toast";
 import SaveData from "./SaveData";
 import { AuthContext } from "../../../../AuthProvider/AuthProvider";
@@ -10,16 +10,26 @@ const DocumentUpload = () => {
   const [UpdatedDocuments, setUpdatedDocuments] = useState(Documents.Data);
   const stepperData = useOutletContext();
   const [isStepperVisible, currentStep, steps, handleStepClick] = stepperData;
-  const { confirmAlert, sendUserDataIntoDB, getApplicationData } = useContext(AuthContext);
+  const { confirmAlert, sendUserDataIntoDB, getApplicationData } =
+    useContext(AuthContext);
+
+  const formData = new FormData();
 
   const handleFileChange = (event, eventId) => {
     const file = event?.target?.files[0];
-    file && toast.success(`${file?.name.slice(0,20)}... uploaded successfully!`);
+    file &&
+      toast.success(`${file?.name.slice(0, 20)}... uploaded successfully!`);
     // Update selectedFiles object with the selected file for the specific question ID.
+    // console.log(localStoreDrawingData, "PREVIOUS GET");
+
+    console.log(eventId, file);
+    // Set File Uploaded Data
     setSelectedFiles((prevSelectedFiles) => ({
       ...prevSelectedFiles,
       [eventId]: file,
     }));
+
+    console.log(selectedFiles, "Selected files");
 
     // Update UpdatedDocuments with the selected file for the specific question ID.
     const updatedData = UpdatedDocuments.map((Question) => {
@@ -29,12 +39,12 @@ const DocumentUpload = () => {
 
     setUpdatedDocuments(updatedData);
   };
+
   console.log(UpdatedDocuments, "UpdatesDocuments");
 
   const applicationNo = JSON.parse(localStorage.getItem("CurrentAppNo"));
 
   useEffect(() => {
-
     const gettingData = async () => {
       let updatedDocumentsToAdd = [];
       const applicationData = await getApplicationData(applicationNo);
@@ -42,12 +52,18 @@ const DocumentUpload = () => {
       if (applicationCheckList.length) {
         // Declare the array here
         applicationCheckList.forEach((data, index) => {
-          const already=UpdatedDocuments.find(document=>document.question===data.question)
-            if(already){
-              return null;
-            }
+          const already = UpdatedDocuments.find(
+            (document) => document.question === data.question
+          );
+          if (already) {
+            return null;
+          }
           if (index >= 8 && data.answer === "yes") {
-            updatedDocumentsToAdd.push({ id: UpdatedDocuments.length + updatedDocumentsToAdd.length + 1, question: data.question, upload: "" });
+            updatedDocumentsToAdd.push({
+              id: UpdatedDocuments.length + updatedDocumentsToAdd.length + 1,
+              question: data.question,
+              upload: "",
+            });
           }
         });
       }
@@ -57,7 +73,27 @@ const DocumentUpload = () => {
     gettingData();
   }, []);
 
-  console.log(UpdatedDocuments, "updatedDocuments")
+  console.log(UpdatedDocuments, "updatedDocuments");
+
+  // handle file upload
+
+  const handleFileUpload = () => {
+    console.log(UpdatedDocuments, "UPDATE DOCUMENTS");
+
+    // find empty field to stop sending data in to the database
+    const findEmptyField = UpdatedDocuments.find(
+      (field) => field?.upload === ""
+    );
+
+    console.log(findEmptyField, "Find empty field");
+
+    if (!findEmptyField) {
+      console.log("No empty");
+    } else {
+      toast.error("Please fill up all the fields");
+    }
+  };
+
   // Sending data to Backend
   const sendDocumentsData = async (url) => {
     return await sendUserDataIntoDB(url, "PATCH", {
@@ -67,38 +103,44 @@ const DocumentUpload = () => {
   };
 
   return (
-    <div className="text-black p-4">
-      <div className="space-y-8 lg:space-y-5">
-        {UpdatedDocuments.map((Question) => {
-          const { id, question } = Question;
-          return (
-            <div
-              key={id}
-              className="w-full px-2 lg:flex items-center space-y-3 lg:space-y-0"
-            >
-              <p className="flex-1">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      className="text-black p-4"
+    >
+      {UpdatedDocuments.map((Question) => {
+        const { id, question } = Question;
+        return (
+          <>
+            <div key={id} className="w-full px-2 mt-10  mb-28">
+              <p className="text-[17px] font-bold">
                 {id}. {question}
               </p>
 
-              <div className="flex items-center">
-                <label
-                  className={`cursor-pointer bg-gray-300 py-2 px-4 rounded-full ${selectedFiles[id]?.name ? "bg-green-500" : "hover:shadow-md"
-                    }`}
+              <div className="flex justify-center items-center mt-10">
+                <input
+                  name={id}
+                  type="file"
+                  accept=".pdf, image/*"
+                  onChange={(event) => handleFileChange(event, id)}
+                  className="file-input file-input-bordered file-input-md w-full max-w-xs"
+                />
+                <Link
+                  to={`https://drive.google.com/file/d//view?usp=sharing`}
+                  target="_blank"
+                  className="hover:underline ms-10 p-3"
                 >
-                  {selectedFiles[id]?.name ? "Uploaded" : "Upload"}
-                  <input
-                    name={id}
-                    type="file"
-                    accept=".pdf, image/*"
-                    onChange={(event) => handleFileChange(event, id)}
-                    style={{ display: "none" }}
-                  />
-                </label>
+                  View old File
+                </Link>
               </div>
             </div>
-          );
-        })}
-      </div>
+            <div className="divider"></div>
+          </>
+        );
+      })}
+
+      <input onClick={handleFileUpload} type="submit" value="Submit" />
 
       {/* save & continue  */}
       {/* navigation button  */}
@@ -110,7 +152,7 @@ const DocumentUpload = () => {
         confirmAlert={confirmAlert}
         collectInputFieldData={sendDocumentsData}
       />
-    </div>
+    </form>
   );
 };
 
