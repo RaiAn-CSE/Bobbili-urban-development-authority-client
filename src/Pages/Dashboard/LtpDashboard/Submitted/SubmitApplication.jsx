1198fea778269e32740734eaee42f3a0aad603e5
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-09-26 12:09:35 +0600

    Get all submitted application successfully

--- src/Pages/Dashboard/LtpDashboard/Submitted/SubmitApplication.jsx
+++ src/Pages/Dashboard/LtpDashboard/Submitted/SubmitApplication.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useQuery } from "react-query";
+import { AuthContext } from "../../../../AuthProvider/AuthProvider";
 
 const SubmitApplication = () => {
+  const { userInfoFromLocalStorage } = useContext(AuthContext);
+
+  // get all applications which are submitted already
+  const { data, refetch, isLoading, isError } = useQuery(
+    ["allSubmitApplication"],
+    async () => {
+      const response = await fetch(
+        `http://localhost:5000/allSubmitApplications?id=${
+          userInfoFromLocalStorage()._id
+        }`
+      );
+      return await response.json();
+    }
+  );
+
+  console.log(data);
   return (
     <div>
       <p>this is Submit Application</p>
