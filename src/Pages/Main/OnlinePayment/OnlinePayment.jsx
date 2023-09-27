a6b46cb617ebf91c02e122077d82ad9b9801a550
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-09-27 10:40:10 +0600

    application page:data showing done

---
+++ src/Pages/Main/OnlinePayment/OnlinePayment.jsx
@@ -0,0 +1,10 @@
+
+function OnlinePayment() {
+    return (
+        <div>
+          Online payment is currently in progress. Please don't be prompted.
+        </div>
+    )
+}
+
+export default OnlinePayment;
