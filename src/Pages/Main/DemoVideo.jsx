b857ddf3c89226cd6d2f9ad2e5d13bfa56736f91
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-10-05 02:49:17 +0600

    Privacy jsx created

---
+++ src/Pages/Main/DemoVideo.jsx
@@ -0,0 +1,15 @@
+const DemoVideo = () => (
+  <div className="video-responsive">
+    <iframe
+      width="560"
+      height="315"
+      src={``}
+      title="User Demo Video"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+);
+
+export default DemoVideo;
