a69d13679acc508b48a26091d0e9d2320ba8f092
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-10-05 02:37:09 +0600

    working on carousel

--- src/Pages/Main/Carousel/Carousel.jsx
+++ src/Pages/Main/Carousel/Carousel.jsx
@@ -2,34 +2,36 @@ import React from "react";
 import CarouselImgOne from "../../../assets/images/DTCP-1.jpg";
 
 const Carousel = () => {
+  // let currentIndex = 1;  
+  // setInterval(() => {
+  //   if (currentIndex === 4) {
+  //     currentIndex = 1;
+  //   }else{ currentIndex+1;}
+  // }, 3000);
+ 
+
+
   return (
     <div className="h-full">
-      <div className="carousel w-full">
+      <div className="carousel w-full h-full">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+          <img src={CarouselImgOne} className="w-full h-full" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">❮</a>
+            <a href="#slide3" className="btn btn-circle">❮</a>
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+          <img src="https://images.unsplash.com/photo-1516978101789-720eacb59e79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" className="w-full" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">❮</a>
             <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+          <img src="https://images.unsplash.com/photo-1606011334315-025e4baab810?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80" className="w-full" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">❮</a>
-            <a href="#slide4" className="btn btn-circle">❯</a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">❮</a>
             <a href="#slide1" className="btn btn-circle">❯</a>
           </div>
         </div>
