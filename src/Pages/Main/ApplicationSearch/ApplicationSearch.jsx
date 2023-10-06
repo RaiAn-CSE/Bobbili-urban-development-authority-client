0782b53554a3809b42f15560616db9a436049a94
Ami.Raian <ami.robirai@gmail.com>
2023-10-06 17:09:50 +0600

    application search data show added

--- src/Pages/Main/ApplicationSearch/ApplicationSearch.jsx
+++ src/Pages/Main/ApplicationSearch/ApplicationSearch.jsx
@@ -114,24 +114,29 @@ const ApplicationSearch = () => {
               id="surveyNo"
               type="text"
               placeholder="xxxxxxx"
+              // filteredData?.buildingInfo?.generalInformation?.surveyNo
+              ltpDetails={filteredData?.buildingInfo?.generalInformation?.surveyNo}
             />
             <MainPageInput
               label="Village"
               id="village"
               type="text"
               placeholder="xxxxxxx"
+              ltpDetails={filteredData?.buildingInfo?.generalInformation?.village}
             />
             <MainPageInput
               label="Mandal"
               id="mandal"
               type="text"
               placeholder="xxxxxxx"
+              ltpDetails={filteredData?.buildingInfo?.generalInformation?.mandal}
             />
             <MainPageInput
               label="District"
               id="district"
               type="text"
               placeholder="xxxxxxx"
+              ltpDetails={filteredData?.buildingInfo?.generalInformation?.district}
             />
           </div>
 
@@ -181,12 +186,14 @@ const ApplicationSearch = () => {
               id="name1"
               type="text"
               placeholder="xxxxxxx"
+              ltpDetails={filteredData?.applicantInfo.applicantDetails?.[0].name}
             />
             <MainPageInput
               label="Address"
               id="address1"
               type="text"
               placeholder="xxxxxxx"
+              ltpDetails={filteredData?.applicantInfo.applicantDetails?.[0].address}
             />
           </div>
 
@@ -196,12 +203,14 @@ const ApplicationSearch = () => {
               id="name2"
               type="text"
               placeholder="xxxxxxx"
+              ltpDetails={filteredData?.applicantInfo.ltpDetails?.name}
             />
             <MainPageInput
               label="Address"
               id="address2"
               type="text"
               placeholder="xxxxxxx"
+              ltpDetails={filteredData?.applicantInfo.ltpDetails?.address}
             />
           </div>
         </div>
