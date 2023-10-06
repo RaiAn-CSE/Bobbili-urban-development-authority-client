ec0cb64157f91227512f6b9f03a8aaa44b2d097f
Ami.Raian <ami.robirai@gmail.com>
2023-10-06 15:03:31 +0600

    plus button color change

--- src/Pages/Dashboard/LtpDashboard/DraftApplication/FloorDetails.jsx
+++ src/Pages/Dashboard/LtpDashboard/DraftApplication/FloorDetails.jsx
@@ -85,7 +85,7 @@ const FloorDetails = ({
         {index === length - 1 && index < 3 && (
           <div className="flex justify-center items-center">
             <button
-              className="text-xl bg-orange-400 rounded-full w-[30px] h-[30px]"
+              className="text-xl rounded-full w-[30px] h-[30px] bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white cursor-pointer shadow-lg shadow-violetDark transition-all duration-500 hover:shadow-sm hover:shadow-black font-bold"
               onClick={increaseFloorNo}
             >
               +
