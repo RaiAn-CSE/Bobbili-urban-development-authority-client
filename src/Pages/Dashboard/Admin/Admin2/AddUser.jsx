d9b8a2cc46a6f7e1b20fd26b718a7cfd916d12d7
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-10-08 22:42:36 +0600

    Condition wise user create field display completed

--- src/Pages/Dashboard/Admin/Admin2/AddUser.jsx
+++ src/Pages/Dashboard/Admin/Admin2/AddUser.jsx
@@ -1,48 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
 const AddUser = () => {
   const { register, handleSubmit, resetField } = useForm();
 
+  const [userType, setUserType] = useState(null);
+
   const gradientColor = "bg-gradient-to-r from-violet-500 to-fuchsia-500";
   const onSubmit = (data) => {
     console.log(data);
 
-    let userInfo;
-
-    if (data?.role.toLowerCase() === "ltp") {
-      userInfo = { ...data, draftApplication: [], submitApplication: [] };
+    if (data?.role.toLowerCase().includes("select an option")) {
+      toast.error("Please select a role");
     } else {
-      userInfo = { ...data };
-    }
+      let userInfo;
 
-    // store users data in the database
-    fetch("http://localhost:5000/addUser", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(userInfo),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+      if (data?.role.toLowerCase() === "ltp") {
+        userInfo = { ...data, draftApplication: [], submitApplication: [] };
+      } else {
+        userInfo = { ...data };
+      }
 
-        if (data.acknowledged) {
-          toast.success("User added successfully");
-          resetField("name");
-          resetField("userId");
-          resetField("password");
-        }
-        if (data?.result === 0) {
-          console.log(data.message);
-          toast.error(data.message);
-        }
+      // store users data in the database
+      fetch("http://localhost:5000/addUser", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(userInfo),
       })
-      .catch(() => {
-        toast.error("Server is not responded");
-      });
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+
+          if (data.acknowledged) {
+            toast.success("User added successfully");
+            resetField("name");
+            resetField("userId");
+            resetField("password");
+          }
+          if (data?.result === 0) {
+            console.log(data.message);
+            toast.error(data.message);
+          }
+        })
+        .catch(() => {
+          toast.error("Server is not responded");
+        });
+    }
+  };
+
+  const handleChangeRole = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setUserType(e.target.value);
   };
   return (
     <div className="mt-10">
@@ -54,10 +66,10 @@ const AddUser = () => {
       </h1>
 
       <form
-        className="w-1/2 mx-auto mt-10 font-roboto text-lg"
+        className="w-2/3 mx-auto mt-10 font-roboto text-lg"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="mb-8 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="mb-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           <div>
             <label
               htmlFor="name"
@@ -91,9 +103,7 @@ const AddUser = () => {
               required
             />
           </div>
-        </div>
 
-        <div className="mb-8 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
             <label
               htmlFor="password"
@@ -116,15 +126,18 @@ const AddUser = () => {
               htmlFor="role"
               className="block mb-2 text-base font-bold text-gray-900 "
             >
-              Select an option
+              Role
             </label>
             <select
               id="role"
               {...register("role", { required: true })}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              onChange={(e) => handleChangeRole(e)}
             >
+              <option value="Select an option">Select an option</option>
               <option value="LTP">LTP</option>
               <option value="PS">PS</option>
+              <option value="UDA">UDA</option>
               <option value="Super Admin">Super Admin</option>
               <option value="Admin1">Admin1</option>
               <option value="Admin2">Admin2</option>
@@ -132,7 +145,112 @@ const AddUser = () => {
           </div>
         </div>
 
-        <div className="flex justify-center">
+        <div className="mb-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-base font-bold text-gray-900 dark:text-white"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              {...register("email", { required: true })}
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="test@gmail.com"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="phone"
+              className="block mb-2 text-base font-bold text-gray-900 dark:text-white"
+            >
+              Phone no
+            </label>
+            <input
+              type="text"
+              {...register("phone", { required: true })}
+              id="phone"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter phone no"
+              maxLength={10}
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="address"
+              className="block mb-2 text-base font-bold text-gray-900 dark:text-white"
+            >
+              Address
+            </label>
+            <textarea
+              id="address"
+              rows="2"
+              {...register("address", { required: true })}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter address..."
+            ></textarea>
+          </div>
+        </div>
+
+        {userType === "LTP" && (
+          <div className="mb-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div>
+              <label
+                htmlFor="licenseNo"
+                className="block mb-2 text-base font-bold text-gray-900 dark:text-white"
+              >
+                License No
+              </label>
+              <input
+                type="text"
+                {...register("licenseNo", { required: true })}
+                id="licenseNo"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter license no"
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="adharNo"
+                className="block mb-2 text-base font-bold '' text-gray-900 dark:text-white"
+              >
+                Adhar no
+              </label>
+              <input
+                type="text"
+                {...register("adharNo", { required: true })}
+                id="adharNo"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter phone no"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="validity"
+                className="block mb-2 text-base font-bold '' text-gray-900 dark:text-white"
+              >
+                Validity
+              </label>
+              <input
+                type="date"
+                {...register("validity", { required: true })}
+                id="validity"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
+          </div>
+        )}
+
+        <div className="flex justify-center my-10">
           <input
             type="submit"
             className={`text-white ${gradientColor} cursor-pointer font-bold rounded-full text-base  sm:w-auto px-10  py-2.5 text-center`}
