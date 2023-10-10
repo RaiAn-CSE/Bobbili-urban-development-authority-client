04115271ce709bb5d9e507ef4064d02d0e73f8a6
Ami.Raian <ami.robirai@gmail.com>
2023-10-11 00:42:20 +0600

    image upload option done

--- src/Pages/Dashboard/PsDashboard/InwardApplications/ImageUploadInput.jsx
+++ src/Pages/Dashboard/PsDashboard/InwardApplications/ImageUploadInput.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ImageUploadInput = ({ id }) => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    // Function to handle file selection
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setSelectedFile(file);
+    };
+
     return (
         <div className="flex items-center justify-center w-full">
-            <label for={id} className="flex flex-col items-center justify-center w-full cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+            <label htmlFor={id} className="flex flex-col items-center justify-center w-full cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                 <div className="flex flex-col items-center justify-center p-[10px]">
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                    {selectedFile ? (
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                            <span className="font-semibold">Uploaded file: </span>{selectedFile.name}
+                        </p>
+                    ) : (
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                            <span className="font-semibold">Click to upload</span> or drag and drop
+                        </p>
+                    )}
                 </div>
-                <input id={id} type="file" className="hidden" />
+                <input
+                    id={id}
+                    type="file"
+                    className="hidden"
+                    onChange={handleFileChange}
+                />
             </label>
         </div>
     );
