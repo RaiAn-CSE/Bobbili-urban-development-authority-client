4a13df46bca7a0ed6d31e6f86ef0721463dee851
Ami.Raian <ami.robirai@gmail.com>
2023-10-14 19:35:06 +0600

    image data get and name show

--- src/Pages/Dashboard/PsDashboard/InwardApplications/ImageUploadInput.jsx
+++ src/Pages/Dashboard/PsDashboard/InwardApplications/ImageUploadInput.jsx
@@ -1,34 +1,26 @@
 import React, { useState } from "react";
 
-const ImageUploadInput = ({
-    id,
-    siteInspectionValue,
-    handleFileChange,
-    siteBoundariesImageFiles,
-}) => {
-    // const [selectedFile, setSelectedFile] = useState(null);
+const ImageUploadInput = ({ id, onFileChange, siteBoundariesImageFiles }) => {
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        onFileChange(id, file);
+    };
 
-    // const handleFileChange = (e) => {
-    //     const file = e.target.files[0];
-    //     // console.log(file, id);
-    //     setSelectedFile(file);
-    // };
     return (
         <div className="flex items-center justify-center w-full">
             <label
                 htmlFor={id}
-                className="flex flex-col items-center justify-center w-full cursor-pointer bg-gray-50 hover:bg-gray-100  dark:bg-gray-700  dark:hover:bg-gray-600"
+                className="flex flex-col items-center justify-center w-full cursor-pointer bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600"
             >
                 <div className="flex flex-col items-center justify-center p-[10px]">
-                    {siteBoundariesImageFiles ? (
+                    {siteBoundariesImageFiles && siteBoundariesImageFiles[id] ? (
                         <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
                             <span className="font-semibold">Uploaded file: </span>
-                            {siteBoundariesImageFiles.name}
+                            {siteBoundariesImageFiles[id].name}
                         </p>
                     ) : (
                         <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                            <span className="font-semibold">Click to upload</span> or drag and
-                            drop
+                            <span className="font-semibold">Click to upload</span> or drag and drop
                         </p>
                     )}
                 </div>
@@ -36,9 +28,7 @@ const ImageUploadInput = ({
                     id={id}
                     type="file"
                     className="hidden"
-                    onChange={(e) => handleFileChange(e, id)}
-                    //   onChange={handleFileChange}
-                    value={siteInspectionValue}
+                    onChange={handleFileChange}
                 />
             </label>
         </div>
--- src/Pages/Dashboard/PsDashboard/InwardApplications/SiteInspection.jsx
+++ src/Pages/Dashboard/PsDashboard/InwardApplications/SiteInspection.jsx
@@ -65,24 +65,6 @@ const SiteInspection = () => {
     getData();
   }, []);
 
-  // let [formObject, setFormObject] = useState({ natureOfSite: "", siteLevel: "" })
-
-  // const inputOnChange = (property, value) => {
-
-  //     setFormObject(property => ({
-  //         ...property,
-  //         [property]: value
-  //     }))
-  // }
-
-  // const formSubmit = (e) => {
-  //     e.preventDefault();
-  //     console.log(formObject);
-
-  // }
-
-  // Get SiteInspection Data :
-
   // Decision :
   const [radioPs, setRadioPs] = useState("");
   const handleRadioPs = (e) => {
@@ -90,16 +72,18 @@ const SiteInspection = () => {
   };
 
 
-  const [siteBoundariesImageFiles, setSiteBoundariesImageFiles] = useState({
-    northApp: "",
-    northObs: "",
-    southApp: "",
-    southObs: "",
-    eastApp: "",
-    eastObs: "",
-    westApp: "",
-    westObs: "",
-  });
+  // const [siteBoundariesImageFiles, setSiteBoundariesImageFiles] = useState({
+  //   northApp: "",
+  //   northObs: "",
+  //   southApp: "",
+  //   southObs: "",
+  //   eastApp: "",
+  //   eastObs: "",
+  //   westApp: "",
+  //   westObs: "",
+  // });
+
+  // console.log(siteBoundariesImageFiles, 'siteBoundariesImageFiles');
 
   // const get = () => {
   //   const northApp = document.getElementById("northApp").files[0];
@@ -124,11 +108,12 @@ const SiteInspection = () => {
   // };
 
 
+  const [siteBoundariesImageFiles, setSiteBoundariesImageFiles] = useState({});
+
   const handleFileChange = (id, file) => {
-    // Update the siteBoundariesImageFiles object
     setSiteBoundariesImageFiles((prevFiles) => ({
       ...prevFiles,
-      [id]: file, // Update the specific property with the provided ID
+      [id]: file,
     }));
   };
 
@@ -431,14 +416,14 @@ const SiteInspection = () => {
                   <td className="whitespace-nowrap border-r border-neutral-500">
                     <ImageUploadInput
                       id="northApp"
-                      handleFileChange={(file) => handleFileChange("northApp", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
                   <td className={inputTableDataClass}>
                     <ImageUploadInput
                       id="northObs"
-                      handleFileChange={(file) => handleFileChange("northObs", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
@@ -448,14 +433,14 @@ const SiteInspection = () => {
                   <td className={inputTableDataClass}>
                     <ImageUploadInput
                       id="southApp"
-                      handleFileChange={(file) => handleFileChange("southApp", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
                   <td className={inputTableDataClass}>
                     <ImageUploadInput
                       id="southObs"
-                      handleFileChange={(file) => handleFileChange("southObs", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
@@ -465,14 +450,14 @@ const SiteInspection = () => {
                   <td className={inputTableDataClass}>
                     <ImageUploadInput
                       id="eastApp"
-                      handleFileChange={(file) => handleFileChange("eastApp", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
                   <td className={inputTableDataClass}>
                     <ImageUploadInput
                       id="eastObs"
-                      handleFileChange={(file) => handleFileChange("eastObs", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
@@ -482,14 +467,14 @@ const SiteInspection = () => {
                   <td className={inputTableDataClass}>
                     <ImageUploadInput
                       id="westApp"
-                      handleFileChange={(file) => handleFileChange("westApp", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
                   <td className={inputTableDataClass}>
                     <ImageUploadInput
                       id="westObs"
-                      handleFileChange={(file) => handleFileChange("westObs", file)}
+                      onFileChange={handleFileChange}
                       siteBoundariesImageFiles={siteBoundariesImageFiles}
                     />
                   </td>
