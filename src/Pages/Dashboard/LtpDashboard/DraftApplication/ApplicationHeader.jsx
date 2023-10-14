d99916bc8576dd10ab369b8a3dc5edcaf01fe9da
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-10-14 18:07:19 +0600

    updating application page

--- src/Pages/Dashboard/LtpDashboard/DraftApplication/ApplicationHeader.jsx
+++ src/Pages/Dashboard/LtpDashboard/DraftApplication/ApplicationHeader.jsx
@@ -5,40 +5,23 @@ function ApplicationHeader() {
     const day = currentDate.getDate();
     const month = currentDate.getMonth() + 1; // Adding 1 because months are zero-indexed
     const year = currentDate.getFullYear();
-    
-    const monthString = month < 10 ? "0" + month : month.toString();
-    const FormDate = [...day.toString(), ...monthString, ...year.toString()];
-    
+
+
+
 
     const header = ["File No.", "ULB ABBREVIATION", "BP", "ZONE", "CIR", "WD", "", "", "", ""];
 
     return (
         <div className='my-5'>
-            <div className='text-center font-bold'>
-                <h2>FORM - 6</h2>
-                <h2 className='mt-3'>………………….(ULB)</h2>
-                <h2 className='mt-3'>BUILDING PERMISSION APPLICATION</h2>
+            <div className='text-center font-extrabold '>
+                <h2 className='mt-3 underline'>BUILDING PERMISSION APPLICATION</h2>
             </div>
-            <div className='mt-6'>
-                <table className="table border-0">
-                    <tbody>
-                        <div className='w-full flex items-center justify-between font-bold'>
-                            <tr>
-                                {header.map(data => <td className='bg-green-200 border border-black px-2 h-10'>{data}</td>)}
-                            </tr>
-                            <tr>
-                                <td className='bg-green-200 border border-black px-2 h-10'>
-                                    Date:</td> {FormDate.map(data => <td className='bg-green-200 border border-black px-2 h-10'>{data}</td>)}
-                            </tr>
-                        </div>
-                    </tbody>
-                </table>
+            <div className='mt-6 flex justify-between'>
+                <p className='font-bold'>Application No.</p>
+                <p> <span className='font-bold'>Date of Submission:</span> {`${day}/${month}/${year}`}</p>
             </div>
             <div className='mt-5'>
-                <h1 className='font-bold'>To</h1>
-                <p>The Commissioner,</p>
-                <p className='mt-3'>……………………………………………..(ULB)</p>
-                <span>(Use CAPITAL LETTERS only)</span>
+                <p>Sub: Building Application for grant of permission for Construction of Building Permission Notice u/s 209 & 227 of APM Act, 1965 / 428 & 433 of HMC Act, 1955.</p>
             </div>
         </div>
     )
