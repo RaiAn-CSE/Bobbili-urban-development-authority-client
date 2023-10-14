9b9684a0c8a30503058eb06372f84ed0387d2ab3
Ami.Raian <ami.robirai@gmail.com>
2023-10-14 14:23:05 +0600

    applicant info page plus minus button alignment

--- src/Pages/Dashboard/LtpDashboard/DraftApplication/OwnerDetail.jsx
+++ src/Pages/Dashboard/LtpDashboard/DraftApplication/OwnerDetail.jsx
@@ -127,14 +127,14 @@ const OwnerDetail = ({
           {index === length - 1 && index < 4 && (
             <div className="flex justify-center items-center">
               <button
-                className="text-xl rounded-full w-[30px] h-[30px] bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white cursor-pointer shadow-lg shadow-violetDark transition-all duration-500 hover:shadow-sm hover:shadow-black font-bold"
-                onClick={increaseApplicantNo}
+                className="text-xl mx-2 rounded-full w-[30px] h-[30px] bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white cursor-pointer shadow-lg shadow-violetDark transition-all duration-500 hover:shadow-sm hover:shadow-black font-bold"
+                onClick={decreaseApplicationNo}
               >
-                +
+                -
               </button>
               <button
-                className="text-xl rounded-full w-[30px] h-[30px] bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white cursor-pointer shadow-lg shadow-violetDark transition-all duration-500 hover:shadow-sm hover:shadow-black font-bold"
-                onClick={decreaseApplicationNo}
+                className="text-xl mx-2 rounded-full w-[30px] h-[30px] bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white cursor-pointer shadow-lg shadow-violetDark transition-all duration-500 hover:shadow-sm hover:shadow-black font-bold"
+                onClick={increaseApplicantNo}
               >
                 +
               </button>
