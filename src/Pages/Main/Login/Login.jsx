4875ceb1e961e960e7bc674f1c8f1b2b6afd5349
Ami.Raian <ami.robirai@gmail.com>
2023-10-31 10:36:56 +0600

    Login page design modified

--- src/Pages/Main/Login/Login.jsx
+++ src/Pages/Main/Login/Login.jsx
@@ -241,7 +241,7 @@ const Login = () => {
               ) : (
                 <motion.div
                   whileHover={{ scale: 1.1 }}
-                // whileTap={{ scale: 0.9 }}
+                  whileTap={{ scale: 0.9 }}
                 >
                   <input
                     type="submit"
