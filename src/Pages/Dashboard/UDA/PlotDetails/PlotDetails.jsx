8ac8ecc945de0ee4546714751dcca08970e1b06a
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-11-02 08:15:37 +0600

    Fetch of plot details table data completed successfully

--- src/Pages/Dashboard/UDA/PlotDetails/PlotDetails.jsx
+++ src/Pages/Dashboard/UDA/PlotDetails/PlotDetails.jsx
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../../AuthProvider/AuthProvider";
 
 const PlotDetails = () => {
+  const { fetchDataFromTheDb } = useContext(AuthContext);
+  const [tableData, setTableData] = useState({});
+
+  const tableHeader = [
+    "S.NO",
+    "FILE NO",
+    "DISTRICT",
+    "MANDAL",
+    "VILLAGE",
+    "OWNER NAME",
+    "OWNER ADDRESS",
+    "LTP NAME",
+    "LTP MOBILE",
+    "MARKET VALUE",
+    "APPROVED DATE",
+    "Total site area",
+    "Road widening area",
+    "NET SITE AREA",
+    "No. of floors",
+    "Builtup area",
+    "No. of UNITS",
+    "Submission Date",
+    "FY",
+    "Year",
+    "Status",
+  ];
+
+  useEffect(() => {
+    (async function () {
+      const applicationData = await fetchDataFromTheDb(
+        "http://localhost:5000/totalApplications"
+      );
+      console.log(applicationData, "AD");
+      if (Object.keys(applicationData)?.length) {
+        setTableData((prev) => {
+          const newValue = {
+            tableHeader,
+            data: applicationData,
+          };
+          return { ...prev, ...newValue };
+        });
+      }
+    })();
+  }, []);
+
+  console.log(tableData, "TABLE DATA");
   return (
     <div>
       <p>PLOT DETAILS</p>
