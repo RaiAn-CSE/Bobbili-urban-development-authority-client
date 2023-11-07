48ba479a88ffd2dab4dca9e30f8e205519bec9a5
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-11-07 11:02:58 +0600

    Add location page design modified

--- src/Pages/Dashboard/Admin/SuperAdmin/UpdateLocation.jsx
+++ src/Pages/Dashboard/Admin/SuperAdmin/UpdateLocation.jsx
@@ -9,6 +9,7 @@ import toast from "react-hot-toast";
 import districtImage from "../../../../assets/images/district.png";
 import cityImage from "../../../../assets/images/city.png";
 import villageImage from "../../../../assets/images/village.png";
+import LocationStyle from "./LocationPageStyle.module.css";
 
 const UpdateLocation = () => {
   const [isAddOption, setIsAddOption] = useState(1);
@@ -24,10 +25,9 @@ const UpdateLocation = () => {
   // const inputClass =
   //   "bg-gradient-to-r from-violet-500 to-fuchsia-500 px-6 py-2 font-bold text-white rounded-full cursor-pointer hover:shadow-md hover:shadow-darkViolet hover:bg-gradient-to-l transition-all duration-700";
   const inputClass =
-    "bg-gradient-to-r from-violet-500 to-fuchsia-500 px-6 py-2 font-bold text-white rounded-full cursor-pointer shadow-lg hover:shadow-darkViolet hover:bg-gradient-to-l transition-all duration-700";
+    "nm_Container bg-gradient-to-r from-violet-500 to-fuchsia-500 px-6 py-2 font-bold text-white rounded-full cursor-pointer  transition-all duration-700";
 
-  const activeTabClass =
-    "bg-gradient-to-r from-teal-400 to-yellow-200 text-black";
+  const activeTabClass = "text-normalViolet text-base  border-b-normalViolet";
 
   const swapTheTab = (tabValue) => {
     setIsAddOption(tabValue);
@@ -116,72 +116,80 @@ const UpdateLocation = () => {
   };
 
   return (
-    <div className="flex justify-center items-center font-roboto h-[calc(100vh-70px)] transition-all duration-1000">
-      {/* container of main div  */}
-      <div className="flex min-h-[70%] w-[80%] bg-white shadow-lg">
-        {/* left side  */}
-        <div className="basis-1/2 bg-gray-100 min-h-full shadow-lg">
-          <div>
-            {/* tab part  */}
-            <ul className="flex text-base font-medium text-center text-gray-500 dark:text-gray-400 bg-gray-100 shadow-md">
-              <li
-                className={`basis-1/2 ${isAddOption === 1 && activeTabClass}`}
-                onClick={() => swapTheTab(1)}
-              >
-                <button className="inline-flex items-center justify-center text-lg font-bold p-4 border-b-2 border-transparent rounded-t-lg">
-                  <MdOutlineAddLocationAlt className="me-2" size={20} />
-                  Add
-                </button>
-              </li>
-              <li
-                className={`basis-1/2 ${isAddOption === 0 && activeTabClass}`}
-                onClick={() => swapTheTab(0)}
-              >
-                <button className="inline-flex items-center justify-center text-lg font-bold p-4 ">
-                  <MdOutlineWrongLocation className="me-2" size={20} />
-                  Delete
-                </button>
-              </li>
-            </ul>
-
-            {/* content part  */}
-
-            <form
-              className="p-6 "
-              onSubmit={(e, isAddOption) => updateLocation(e, isAddOption)}
-            >
-              {/* district */}
-              <div>
-                <label
-                  htmlFor="district"
-                  className="block mb-2 text-base font-bold text-gray-900 dark:text-white"
+    <>
+      <p className="text-black text-2xl font-roboto font-bold ml-3 mt-3">
+        Location:
+      </p>
+      <div className="flex justify-center items-center font-roboto h-[calc(100vh-150px)] transition-all duration-1000">
+        {/* container of main div  */}
+        <div className="flex min-h-[70%] w-[80%]  nm_Container">
+          {/* left side  */}
+          <div className="basis-1/2 bg-bgColor min-h-full ">
+            <div>
+              {/* tab part  */}
+              <ul className="flex text-center text-[#ab9ba0]">
+                <li
+                  className={`basis-1/2 ${
+                    isAddOption === 1 ? activeTabClass : "border-b-white"
+                  } border-2`}
+                  onClick={() => swapTheTab(1)}
                 >
-                  District
-                </label>
-                <div className="relative mb-6">
-                  <div className="absolute inset-y-0 left-0 flex items-center p-3 pointer-events-none">
-                    <img
-                      className="w-full h-full"
-                      src={districtImage}
-                      alt="An image which is representing district"
+                  <button className="inline-flex items-center justify-center text-base p-4 ">
+                    <MdOutlineAddLocationAlt className="me-2" size={20} />
+                    Add
+                  </button>
+                </li>
+                <li
+                  className={`basis-1/2  border-2 ${
+                    isAddOption === 0 ? activeTabClass : "border-b-white"
+                  }`}
+                  onClick={() => swapTheTab(0)}
+                >
+                  <button className="inline-flex items-center justify-center text-base p-4">
+                    <MdOutlineWrongLocation className="me-2" size={20} />
+                    Delete
+                  </button>
+                </li>
+              </ul>
+
+              {/* content part  */}
+
+              <form
+                className="p-6 "
+                onSubmit={(e, isAddOption) => updateLocation(e, isAddOption)}
+              >
+                {/* district */}
+                <div>
+                  <label
+                    htmlFor="district"
+                    className="block mb-2 text-base text-black"
+                  >
+                    District
+                  </label>
+                  <div className="relative mb-6">
+                    <div className="absolute inset-y-0 left-0 flex items-center p-3 pointer-events-none">
+                      <img
+                        className="w-full h-full"
+                        src={districtImage}
+                        alt="An image which is representing district"
+                      />
+                    </div>
+                    <input
+                      type="text"
+                      id="district"
+                      name="district"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5 nm_Inset focus:outline-none"
+                      placeholder="Enter district name"
+                      required
                     />
                   </div>
-                  <input
-                    type="text"
-                    id="district"
-                    name="district"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5"
-                    placeholder="Enter district name"
-                    required
-                  />
                 </div>
-              </div>
 
-              {/* mandal  */}
-              <div>
-                <label
+                {/* mandal  */}
+                <div>
+                  {/* <label
                   htmlFor="mandal"
-                  className="flex items-center mb-2  text-base font-bold text-gray-900 dark:text-white"
+                  className="flex items-center mb-2 text-base text-black"
                 >
                   <input
                     type="checkbox"
@@ -191,33 +199,49 @@ const UpdateLocation = () => {
                     }
                   />
                   Mandal
-                </label>
-                {isMandalNeed === 1 && (
-                  <div className={`relative mb-6 `}>
-                    <div className="absolute inset-y-0 left-0 flex items-center p-3 pointer-events-none">
-                      <img
-                        className="w-full h-full"
-                        src={cityImage}
-                        alt="An image which is representing district"
+                </label> */}
+                  <div className={`${LocationStyle.switchHolder}`}>
+                    <div className={`${LocationStyle.switchLabel}`}>
+                      <span className="text-black">Mandal</span>
+                    </div>
+                    <div className={`${LocationStyle.switchToggle}`}>
+                      <input
+                        type="checkbox"
+                        id="mandal"
+                        className="transition-all duration-700"
+                        onClick={() =>
+                          setToggleValue(isMandalNeed, setIsMandalNeed)
+                        }
+                      />
+                      <label htmlFor="mandal"></label>
+                    </div>
+                  </div>
+                  {isMandalNeed === 1 && (
+                    <div className={`relative mb-6 `}>
+                      <div className="absolute inset-y-0 left-0 flex items-center p-3 pointer-events-none">
+                        <img
+                          className="w-full h-full"
+                          src={cityImage}
+                          alt="An image which is representing district"
+                        />
+                      </div>
+                      <input
+                        type="text"
+                        id="mandal"
+                        name="mandal"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5 transition-all duration-700 nm_Inset focus:outline-none"
+                        placeholder="Enter mandal name"
+                        required={isMandalNeed && "required"}
                       />
                     </div>
-                    <input
-                      type="text"
-                      id="mandal"
-                      name="mandal"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5"
-                      placeholder="Enter mandal name"
-                      required={isMandalNeed && "required"}
-                    />
-                  </div>
-                )}
-              </div>
+                  )}
+                </div>
 
-              {/* vilage  */}
-              <div>
-                <label
+                {/* vilage  */}
+                <div>
+                  {/* <label
                   htmlFor="village"
-                  className="flex items-center mb-2 text-base font-bold text-gray-900 dark:text-white"
+                  className="flex items-center mb-2 text-base text-black "
                 >
                   <input
                     type="checkbox"
@@ -227,52 +251,68 @@ const UpdateLocation = () => {
                     }
                   />
                   Village
-                </label>
-                {isVillageNeed === 1 && (
-                  <div className={`relative mb-6 `}>
-                    <div className="absolute inset-y-0 left-0 flex items-center p-3 pointer-events-none">
-                      <img
-                        className="w-full h-full"
-                        src={villageImage}
-                        alt="An image which is representing district"
+                </label> */}
+                  <div className={`${LocationStyle.switchHolder}`}>
+                    <div className={`${LocationStyle.switchLabel}`}>
+                      <span className="text-black">Village</span>
+                    </div>
+                    <div className={`${LocationStyle.switchToggle}`}>
+                      <input
+                        type="checkbox"
+                        id="village"
+                        onClick={() =>
+                          setToggleValue(isVillageNeed, setIsVillageNeed)
+                        }
+                      />
+                      <label htmlFor="village"></label>
+                    </div>
+                  </div>
+                  {isVillageNeed === 1 && (
+                    <div className={`relative mb-6 `}>
+                      <div className="absolute inset-y-0 left-0 flex items-center p-3 pointer-events-none">
+                        <img
+                          className="w-full h-full"
+                          src={villageImage}
+                          alt="An image which is representing district"
+                        />
+                      </div>
+                      <input
+                        type="text"
+                        id="village"
+                        name="village"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5 transition-all duration-700 nm_Inset focus:outline-none"
+                        placeholder="Enter village name"
+                        required={isVillageNeed === 1 && "required"}
                       />
                     </div>
-                    <input
-                      type="text"
-                      id="village"
-                      name="village"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5"
-                      placeholder="Enter village name"
-                      required={isVillageNeed === 1 && "required"}
-                    />
-                  </div>
-                )}
-              </div>
+                  )}
+                </div>
 
-              {/* submit button  */}
+                {/* submit button  */}
 
-              <div className="flex justify-center">
-                <input
-                  type="submit"
-                  name="submit"
-                  value={isAddOption === 1 ? "Add" : "Delete"}
-                  className={`${inputClass}`}
-                />
-              </div>
-            </form>
+                <div className="flex justify-center">
+                  <input
+                    type="submit"
+                    name="submit"
+                    value={isAddOption === 1 ? "Add" : "Delete"}
+                    className={`${inputClass}`}
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+
+          {/* right side  */}
+          <div className="basis-1/2 bg-bgColor">
+            <Lottie
+              animationData={mapAnimation}
+              loop={true}
+              className="w-full h-full"
+            />
           </div>
         </div>
-
-        {/* right side  */}
-        <div className="basis-1/2 ">
-          <Lottie
-            animationData={mapAnimation}
-            loop={true}
-            className="w-full h-full"
-          />
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
