d35ea1c2c4d7df16e17308749cbc5961aa0e7efc
Ami.Raian <ami.robirai@gmail.com>
2023-11-16 03:27:58 +0600

    error page update

--- src/Pages/Shared/Error.jsx
+++ src/Pages/Shared/Error.jsx
@@ -7,6 +7,12 @@ const Error = () => {
     <div className="bg-cover bg-center h-screen font-sans" style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
       <div className="min-h-screen relative">
 
+        <div className={`${errorPageCss.ErrorContainer} absolute left-[250px] top-[12px]`}>
+          {[...Array(21)].map((_, i) => (
+            <div key={i} className={errorPageCss.ErrorItem} style={{ '--i': i }}></div>
+          ))}
+        </div>
+
         <div className={`${errorPageCss.planet} right-[200px] top-[75px]`}></div>
 
         <div className="flex flex-col items-center">
@@ -14,7 +20,7 @@ const Error = () => {
           <button className={`${errorPageCss.errorBtn} max-w-[200px] max-h-[70px]`}>Way to home</button>
         </div>
 
-        <p className="text-2xl text-violet-400 font-bold absolute right-[190px] top-[220px]">You are lost</p>
+        <p className="text-2xl border rounded-lg border-violet-600 p-2 text-gray-200 font-bold absolute left-[170px] top-[100px]">You are lost</p>
 
       </div>
     </div>
