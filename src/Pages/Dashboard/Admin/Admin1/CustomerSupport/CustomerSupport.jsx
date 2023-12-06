9cfc967a15900ae6abd134d7564c4f04c1333761
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-12-06 19:12:19 +0600

    Tab system added for checking message request

--- src/Pages/Dashboard/Admin/Admin1/CustomerSupport/CustomerSupport.jsx
+++ src/Pages/Dashboard/Admin/Admin1/CustomerSupport/CustomerSupport.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 const CustomerSupport = () => {
-  return <div>Customer support</div>;
+  return (
+    <div role="tablist" className="tabs tabs-lifted m-3">
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab text-base font-bold checked:text-normalViolet"
+        aria-label="New Request"
+        defaultChecked={true}
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        Tab content 1
+      </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab text-base font-bold checked:text-normalViolet"
+        aria-label="Chat"
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        Tab content 2
+      </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab text-base font-bold checked:text-normalViolet"
+        aria-label="Today"
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        Tab content 3
+      </div>
+    </div>
+  );
 };
 
 export default CustomerSupport;
