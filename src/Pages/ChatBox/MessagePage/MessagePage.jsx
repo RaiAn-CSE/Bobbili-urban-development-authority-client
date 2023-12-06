8ac75439166e0e9363d40ff5a8cf2c609a45ca2d
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-12-06 08:37:19 +0600

    Interval issue and request again functionality and check accept request functionality completed

--- src/Pages/ChatBox/MessagePage/MessagePage.jsx
+++ src/Pages/ChatBox/MessagePage/MessagePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import toast from "react-hot-toast";
 import io from "socket.io-client";
+import axios from "axios";
 
 const socket = io("http://localhost:5000");
 
@@ -9,22 +10,28 @@ const MessagePage = ({ props }) => {
   const [timeEnd, setTimeEnd] = useState(false);
   const [checkUpdateData, setCheckUpdateData] = useState({});
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [isAccepted, setIsAccepted] = useState(false);
+
   console.log(userInfo, "Userinfo");
+
+  function handleCheckAcceptMessage(event) {
+    console.log("Handling check-accept-message:", event.data);
+    // Your logic for handling the check-accept-message event
+  }
+
   useEffect(() => {
     !timeEnd &&
       socket.on("check-accept-message", (data) => {
         // Handle the new data received from the server
         console.log(data, "Data");
-        console.log(userInfo.uniqueId === data.documentKey._id);
-        console.log(userInfo.uniqueId, data.documentKey._id);
-        console.log(userInfo, "Userinfo");
-        if (userInfo.uniqueId === data.documentKey._id) {
-          socket.off("check-accept-message");
-          setCheckUpdateData(data.updateDescription.updateFields);
 
+        console.log(userInfo, "Userinfo");
+        if (userInfo.uniqueId === data.change.documentKey._id) {
+          socket.off("check-accept-message");
+          setCheckUpdateData(data);
+          setIsAccepted(true);
           axios.patch(
-            `http://localhost:5000/messageRequest?update=${JSON.stringigy({
+            `http://localhost:5000/messageRequest?update=${JSON.stringify({
               id: userInfo.uniqueId,
               senderId: data.senderId,
               action: "sendId",
@@ -36,28 +43,42 @@ const MessagePage = ({ props }) => {
     return () => {
       // Clean up event listeners on component unmount
       socket.off("check-accept-message");
-      clearInterval(countDownInterval);
+      //   clearInterval(countDownInterval);
     };
-  }, [socket]);
+  }, [socket, timeEnd]);
+
+  //   let counter = 10;
+  //   const countDownInterval = setInterval(() => {
+  //     if (counter > 0) {
+  //       counter = counter - 1;
+  //       console.log(counter, "INSIde interval");
+  //       //   counter === 0 && setTimeEnd(true);
+  //     }
+
+  //     document
+  //       .getElementById("counterElement")
+  //       .style.setProperty("--value", counter);
+  //   }, 1000);
+
+  const [counter, setCounter] = useState(30);
 
   useEffect(() => {
-    let counter = 10;
     const countDownInterval = setInterval(() => {
       if (counter > 0) {
-        counter = counter - 1;
-        console.log(counter, "INSIde interval");
-        counter === 0 && setTimeEnd(true);
+        setCounter((prevCounter) => prevCounter - 1);
       }
-
-      document
-        .getElementById("counterElement")
-        .style.setProperty("--value", counter);
     }, 1000);
-  }, []);
+
+    // Cleanup function to clear the interval when the component is unmounted
+    return () => {
+      clearInterval(countDownInterval);
+    };
+  }, [counter]);
 
   useEffect(() => {
-    console.log(timeEnd, "timeEnd update");
-    if (setTimeEnd) {
+    if (counter === 0) {
+      setTimeEnd(true);
+      socket.off("check-accept-message");
       console.log("Counter inside");
       fetch(
         `http://localhost:5000/messageRequest?update=${JSON.stringify({
@@ -77,17 +98,53 @@ const MessagePage = ({ props }) => {
           setError("Server Error");
         });
     }
-  }, [timeEnd]);
+  }, [counter]);
 
   console.log("HELLO");
 
+  const requestAgain = async () => {
+    try {
+      const { data } = await axios.patch(
+        `http://localhost:5000/messageRequest?update=${JSON.stringify({
+          id: userInfo.uniqueId,
+          action: "requestAgain",
+        })}`
+      );
+
+      console.log(data, "UPDATE REQ");
+
+      if (data.acknowledged) {
+        setCounter(30);
+        setTimeEnd(false);
+        setCheckUpdateData({});
+        setError("");
+        setIsAccepted(false);
+        socket.on("check-accept-message");
+      }
+    } catch (error) {
+      setError("Server Error");
+    }
+  };
+
   return (
     <div>
-      <div>
-        <span className="countdown">
-          <span id="counterElement"></span>
-        </span>
-      </div>
+      {timeEnd && !isAccepted && (
+        <div>
+          <button className="bg-normalViolet text-white" onClick={requestAgain}>
+            Request Again
+          </button>
+        </div>
+      )}{" "}
+      {!timeEnd && !isAccepted && (
+        <div>
+          <span className="countdown">
+            <span id="counterElement" style={{ "--value": counter }}>
+              {counter}
+            </span>
+          </span>
+        </div>
+      )}
+      {isAccepted && <div>Accept</div>}
     </div>
   );
 };
