3710ee69b46c8f9a67916a4bdbe5e42e837f6299
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-12-07 20:25:08 +0600

    Missed message showed in the front end and delete button worked perfectly

--- src/Pages/Dashboard/Admin/Admin1/CustomerSupport/MissedRequest/MissedRequest.jsx
+++ src/Pages/Dashboard/Admin/Admin1/CustomerSupport/MissedRequest/MissedRequest.jsx
@@ -1,15 +1,65 @@
 import React, { useEffect, useState } from "react";
 import TableLayout from "../../../../../Components/TableLayout";
-import Lottie from "lottie-web";
+import Lottie from "lottie-react";
 import ErrorAnimation from "../../../../../../assets/ServerError.json";
 import ShowMissedRequest from "./ShowMissedRequest";
+import axios from "axios";
+import io from "socket.io-client";
+import toast from "react-hot-toast";
+
+const socket = io("http://localhost:5000");
+
 const MissedRequest = () => {
-  const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
   const [allData, setAllData] = useState([]);
   const [tableData, setTableData] = useState({});
   const [tableComponentProps, setTableComponentProps] = useState({});
-  const tableHeader = ["Sl.no.", "Name", "Contact", "Action"];
+  const [error, setError] = useState("");
+  const tableHeader = ["Sl.no.", "Customer Info", "Action"];
+
+  useEffect(() => {
+    setError("");
+    fetch("http://localhost:5000/missedMessage")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setAllData(result);
+      })
+      .catch((err) => {
+        setError("Server Error");
+      });
+  }, []);
+
+  useEffect(() => {
+    socket.on("check-accept-message", async (data) => {
+      // Handle the new data received from the server
+      console.log(data, "Data");
+
+      if (data?.change?.updatedFields?.noResponse === 1) {
+        setAllData([...allData, data?.changedDocument]);
+      }
+    });
+
+    return () => {
+      // Clean up event listeners on component unmount
+      socket.off("check-accept-message");
+      //   clearInterval(countDownInterval);
+    };
+  }, [socket]);
+
+  const checkedMissedMessage = async (id) => {
+    const filteredData = allData.filter((each) => each._id !== id);
+    setAllData(filteredData);
+    const { data } = await axios.delete(
+      `http://localhost:5000/missedMessage?id=${id}`
+    );
+
+    if (data.acknowledged) {
+      toast.success("Contacted with the person");
+    } else {
+      toast.error("Server Error");
+    }
+  };
 
   useEffect(() => {
     setTableData((prev) => {
@@ -19,7 +69,11 @@ const MissedRequest = () => {
       };
       return { ...prev, ...newValue };
     });
+
+    setTableComponentProps({ checkedMissedMessage });
   }, [allData]);
+  console.log(error);
+
   return (
     <>
       {error?.length !== 0 ? (
@@ -44,7 +98,7 @@ const MissedRequest = () => {
 
             {allData?.length === 0 && (
               <p className="text-center  font-bold text-xl text-black">
-                No application Found
+                No Request Found
               </p>
             )}
 
