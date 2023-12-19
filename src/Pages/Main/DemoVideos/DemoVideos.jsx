581d6f7d0106726a02b29b3390241cd25d9d8f05
Ami.Raian <ami.robirai@gmail.com>
2023-12-19 19:50:26 +0600

    demo videos update

--- src/Pages/Main/DemoVideos/DemoVideos.jsx
+++ src/Pages/Main/DemoVideos/DemoVideos.jsx
@@ -5,7 +5,7 @@ const DemoVideos = () => {
   const videoUrl = "https://youtu.be/aGHCyzVqfrQ?si=Rtrmar90OCI15ylH";
 
   return (
-    <div className="flex text-center w-full p-4">
+    <div className="flex text-center w-full p-4 max-h-[400px] overflow-hidden">
 
       <div className="basis-[70%]">
         <div className="mr-4 relative">
@@ -17,7 +17,21 @@ const DemoVideos = () => {
         </div>
       </div>
 
-      <div className="basis-[30%] overflow-hidden overflow-y-auto">
+      <div className="basis-[30%] overflow-hidden overflow-y-auto rounded-lg">
+        <div className="relative mb-2">
+          <h2 className="absolute top-2 left-2 text-xl font-bold text-white">Video 2 Title</h2>
+          <video className="w-full rounded-lg" controls autoPlay muted>
+            <source src="https://docs.material-tailwind.com/demo.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="relative mb-2">
+          <h2 className="absolute top-2 left-2 text-xl font-bold text-white">Video 2 Title</h2>
+          <video className="w-full rounded-lg" controls autoPlay muted>
+            <source src="https://docs.material-tailwind.com/demo.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
         <div className="relative mb-2">
           <h2 className="absolute top-2 left-2 text-xl font-bold text-white">Video 2 Title</h2>
           <video className="w-full rounded-lg" controls autoPlay muted>
